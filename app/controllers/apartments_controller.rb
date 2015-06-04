bbf2d31293baf598282fc3fcde36ed95bb2416e6
Dmitrii <vkosmos26@yandex.ru>
2015-06-04 20:35:24 +0300

    apartment new

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,16 +1,28 @@
 class ApartmentsController < ApplicationController
   def index
+    @apartment = Apartment.all
   end
 
   def new
   end
 
   def create
+    if Apartment.create!(get_params)
+      redirect_to 'index'
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def get_params
+    params.require(:apartment).permit(:description, :renter,:phone1)    
   end
 end
