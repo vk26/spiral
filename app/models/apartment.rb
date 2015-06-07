3c6c26aeed3122c3d5d49f0213a4ee2bc9f5fbe4
Dmitrii <vkosmos26@yandex.ru>
2015-06-07 18:37:54 +0300

    scope admin and validates phone number

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,4 @@
 class Apartment < ActiveRecord::Base
   validates :renter, :phone1, presence: true 
+  validates :phone1, format: { without: /[a-zA-Zа-яА-Я]/, message: 'Номер телефона не должен содержать буквы'}
 end
