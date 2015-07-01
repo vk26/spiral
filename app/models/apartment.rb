167cfb390ac29fa7b94478fa9bd475e387fbfecd
Dmitrii <vkosmos26@yandex.ru>
2015-07-01 23:21:09 +0800

    init model assets

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,5 @@
 class Apartment < ActiveRecord::Base
+  has_many :assets
   validates :renter, :phone1, presence: true
   validates :phone1, format: { without: /[a-zA-Zа-яА-Я]/, message: I18n.t('.validation.apartment.phone') }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624140425) do
+ActiveRecord::Schema.define(version: 20150701151358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20150624140425) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "assets", force: :cascade do |t|
+    t.integer  "apartment_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "assets", ["apartment_id"], name: "index_assets_on_apartment_id", using: :btree
+
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
@@ -55,4 +63,5 @@ ActiveRecord::Schema.define(version: 20150624140425) do
 
   add_foreign_key "apartments", "areas"
   add_foreign_key "apartments", "cities"
+  add_foreign_key "assets", "apartments"
 end
