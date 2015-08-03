d9678419880d57e35096865bf112759ebc1628e5
Dmitrii <vkosmos26@yandex.ru>
2015-08-03 20:58:31 +0800

    collections const appartment"

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,7 @@
 class Apartment < ActiveRecord::Base
+  TYPES = %w["1к", "2к", "3к", "4к и более", "к", "койк", "общ", "Неблагоустр. дом", "Благоустр. дом", "посуточно", "неизвестно", "Пансионат", "Гостинка", "Малосемейка", "Студия", "Жилой гараж"]
+  SOURSE = %w["OWN", "AVITO", "IRR"]
+  REPAIR = %w["обычный", "евро", "черновой"]
   has_many :assets, :dependent => :destroy
   accepts_nested_attributes_for :assets, allow_destroy: true
   validates :renter, :phone1, presence: true
